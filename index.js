37f3c947ba0082b17d7b8f694e4f52efbd0818a8
Wendell Santos <wendellsantooos@gmail.com>
2021-12-05 19:59:21 -0300

    Iniciando JS

---
+++ index.js
@@ -0,0 +1,8 @@
+function verificar(){
+    var nome = document.getElementById('pessoa')
+    var nasc = document.getElementById('nasc')
+    var resposta = document.getElementById('res')
+    var img = document.getElementById('imagem')
+    
+    var data = new Date()
+    var anoAtual = data.getFullYear()
