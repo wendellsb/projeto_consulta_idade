54d7b101f46dccb34906d5f80ad1a37abf9db8c1
Wendell Santos <wendellsantooos@gmail.com>
2021-12-05 20:02:35 -0300

    Atualizando JS

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function verificar(){
     
     var data = new Date()
     var anoAtual = data.getFullYear()
+
+    /* Teste de informações */
+    if (nome.value.length == '') {
+        window.alert('Nome inválido!')
+    } else if (nasc.value > anoAtual || nasc.value.length == 0 || (anoAtual - nasc.value) > 120) {
+        window.alert('Data de nascimento inválida!')
+    } else {
+        var sexo = document.getElementsByName('sexo')
+        var cor = document.getElementsByName('cor')
+        var idade = anoAtual - nasc.value
+        var genero = ''
+            
