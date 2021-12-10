4510a11246105e5b6097f9169f97619e60b8dde8
Wendell Santos <wendellsantooos@gmail.com>
2021-12-10 14:00:24 -0300

    10/12

--- index.js
+++ index.js
@@ -75,3 +75,14 @@ function verificar(){
                 img.src = 'idoso_mulher_branco.png'
             }
         } else if(genero == 'feminino' && cor[1].checked){
+            //negra
+            if(idade >= 0 && idade < 6){
+                img.src='bebe_mulher_negro.png'
+            } else if(idade >= 6 && idade < 18){
+                img.src='adolecente_mulher_negro.png'
+            }else if(idade >= 18 && idade < 60){
+                img.src='adulto_mulher_negro.png'
+            }else if(idade >= 60 && idade < 120){
+                img.src = 'idoso_mulher_negro.png'
+            }
+        }else if(genero == 'feminino' && cor[2].checked){
